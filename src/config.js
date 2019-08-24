626dc2267521175920eefad29eb10d21636eac58
Brandon El Haj <brandon.elhaj@gmail.com>
2019-08-24 01:21:06 -0700

    Updated

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
-    NODE_ENV: process.env.NODE_ENV || 'development npm start',
+    NODE_ENV: process.env.NODE_ENV || 'development',
     DB_URL: process.env.DB_URL || 'postgresql://rod@localhost/knowyourgov',
-    JWT_SECRET: process.env.JWT_SECRET || 'rodthebod',
-    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "http://localhost:3000/api"
+    JWT_SECRET: process.env.JWT_SECRET || 'rodthebod'
 }
