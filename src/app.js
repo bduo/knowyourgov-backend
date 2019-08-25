34c2ba405df8f9de380184053f86267152ee187f
Brandon El Haj <brandon.elhaj@gmail.com>
2019-08-25 16:04:08 -0700

    Updated App.js

--- src/app.js
+++ src/app.js
@@ -25,12 +25,12 @@ app.use('/api/users', govUsersRouter)
 
 if (process.env.NODE_ENV === 'production') {
     // Express will serve up production assets
-    app.use(express.static('knowyourgov/build'));
+    app.use(express.static('client/build'));
   
     // Express serve up index.html file if it doesn't recognize route
     const path = require('path');
     app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'knowyourgov', 'build', 'index.html'));
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
   }
 
