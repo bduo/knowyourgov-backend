acbd945121819f1ba196f70f2ae47701ab335184
Brandon El Haj <brandon.elhaj@gmail.com>
2019-08-25 16:25:39 -0700

    Latest Version

--- src/app.js
+++ src/app.js
@@ -23,17 +23,6 @@ app.get('/', (req, res) => {
 app.use('/api/authorization', authRouter)
 app.use('/api/users', govUsersRouter)
 
-// if (process.env.NODE_ENV === 'production') {
-//     // Express will serve up production assets
-//     app.use(express.static('client/build'));
-  
-//     // Express serve up index.html file if it doesn't recognize route
-//     const path = require('path');
-//     app.get('*', (req, res) => {
-//       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//     });
-//   }
-
 app.use(function errorHandler(error, req, res, next) {
     let response
     if (NODE_ENV === 'production') {
@@ -45,6 +34,4 @@ app.use(function errorHandler(error, req, res, next) {
     res.status(500).json(response)
 })
     
-
-
 module.exports = app
